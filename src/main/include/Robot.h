df7b9bcd9da51fcec50f0ba708e8405a208500c8
SpeedRobotics3542 <77466700+SpeedRobotics3542@users.noreply.github.com>
2021-03-13 14:34:16 -0500

    deleted commet

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-#pragma once            // this is a comment
+#pragma once
 
 #include <string>
 #include <frc/TimedRobot.h>
